25a3c6293c62ca983e973ddafa49294862b1c594
Punit Jain <punit.jain@simplybusiness.co.uk>
2020-07-23 18:38:51 +0100

    done some basic set up

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../lib/action'
 
 describe Action do
-  it 'get the version' do
+  it 'returns the correct version for master branch' do
     expect(Action.new.get_version(ref: 'master')).to eq('1.0.0')
   end
 end
