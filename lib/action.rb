356101a42c964e511a09e12bc82e572cb1a1c798
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2020-07-24 16:42:48 -0400

    fetch files changed for a PR

--- lib/action.rb
+++ lib/action.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
+require 'octokit'
 
 class Action
-  def get_version(ref:)
+  attr_reader :client, :repo
 
+  def initialize(repo:, client: nil)
+    @client = client || Octokit::Client.new(access_token: ENV['XBOT_TOKEN'])
+    @repo = "simplybusiness/#{repo}"
+  end
+
+  def version_changed?(pull_number)
+    client.pull_request_files(repo, pull_number).any? do |file_name|
+      /version\.rb/.match(file_name)
+    end
   end
 end
--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -2,7 +2,18 @@ require 'rspec'
 require_relative '../lib/action'
 
 describe Action do
-  it 'returns the correct version for master branch' do
-    expect(Action.new.get_version(ref: 'master')).to eq('1.0.0')
+  let(:client) { instance_double(Octokit::Client) }
+  let(:action) { Action.new(repo: 'test', client: client) }
+
+  describe '#version_changed?' do
+    it 'is truthy if the github API response includes a version file' do
+      allow(client).to receive(:pull_request_files).and_return(['version.rb', 'foo.txt'])
+      expect(action.version_changed?(1)).to be_truthy
+    end
+
+    it 'is falsey if the github API response does not include a version file' do
+      allow(client).to receive(:pull_request_files).and_return(['foo.txt'])
+      expect(action.version_changed?(1)).to be_falsey
+    end
   end
 end
