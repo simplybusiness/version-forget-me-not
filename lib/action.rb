cdf57b214b95ed25b66a688722b34425d8e7b3d1
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2020-07-28 14:42:59 -0400

    version comparison first implementation

--- lib/action.rb
+++ lib/action.rb
@@ -14,8 +14,15 @@ class Action
     @repo = "#{owner}/#{repo_name}"
   end
 
-  def version_changed?(pull_number)
+  def version_file_changed?(pull_number)
     client.pull_request_files(repo, pull_number).include?(ENV['VERSION_FILE_PATH'])
+  end
+
+  def version_updated?(branch:, trunk: 'master')
+    compare_version_arrays(
+        fetch_version(ref: branch).split('.').map(&:to_i),
+        fetch_version(ref: trunk).split('.').map(&:to_i)
+    )
   end
 
   private
@@ -23,5 +30,11 @@ class Action
   def fetch_version(ref:)
     content = client.contents(repo, path: ENV['VERSION_FILE_PATH'], query: { ref: ref })
     content.match(SEMVER_VERSION)[0].gsub(/\'/, '')
+  end
+
+  # returns true if the branch version is greater than the trunk version
+  def compare_version_arrays(branch, trunk)
+    index = (0..2).find { |i| branch[i] != trunk[i] }
+    branch[index] > trunk[index] if index
   end
 end
--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -8,35 +8,67 @@ describe Action do
   let(:action) { Action.new(owner: 'simplybusiness', repo_name: 'test', client: client) }
   before { ENV['VERSION_FILE_PATH'] = 'version.rb' }
 
-  describe '#version_changed?' do
+  def mock_version_response(branch, version)
+    content = %(
+        module TestRepo
+          VERSION='#{version}'
+        end
+      )
+
+    allow(client).to receive(:contents).with(
+        'simplybusiness/test',
+        path: ENV['VERSION_FILE_PATH'],
+        query: { ref: branch }
+    ).and_return(content)
+  end
+
+  describe '#version_file_changed?' do
     it 'return true if the github API response includes a version file' do
       allow(client).to receive(:pull_request_files).and_return(%w[version.rb foo.txt])
-      expect(action.version_changed?(1)).to be true
+      expect(action.version_file_changed?(1)).to be true
     end
 
     it 'return false if the github API response does not include a version file' do
       allow(client).to receive(:pull_request_files).and_return(['foo.txt'])
-      expect(action.version_changed?(1)).to be false
+      expect(action.version_file_changed?(1)).to be false
     end
   end
 
-  describe '#fetch_version' do
-    let(:content) do
-      %(
-        module TestRepo
-          VERSION='1.2.3'
-        end
-      )
+  describe 'version_updated?' do
+    it 'returns false if the versions match' do
+      mock_version_response('master', '1.2.3')
+      mock_version_response('my_branch', '1.2.3')
+
+      expect(action.version_updated?(branch: 'my_branch')).to be_falsey
     end
 
-    it 'reads the version file and return the version for a branch' do
-      allow(client).to receive(:contents).with(
-        'simplybusiness/test',
-        path: ENV['VERSION_FILE_PATH'],
-        query: { ref: 'master' }
-      ).and_return(content)
+    it 'returns false if the branch is behind the trunk' do
+      mock_version_response('master', '1.2.3')
+      mock_version_response('my_branch', '1.1.3')
 
-      expect(action.send(:fetch_version, ref: 'master')).to eq('1.2.3')
+      expect(action.version_updated?(branch: 'my_branch')).to be_falsey
+    end
+
+
+    it 'returns true for a patch version bump' do
+      mock_version_response('master', '1.2.3')
+      mock_version_response('my_branch', '1.2.4')
+
+      expect(action.version_updated?(branch: 'my_branch')).to be_truthy
+    end
+
+    it 'returns true for a minor version bump' do
+      mock_version_response('master', '1.2.3')
+      mock_version_response('my_branch', '1.3.0')
+
+      expect(action.version_updated?(branch: 'my_branch')).to be_truthy
+    end
+
+    it 'returns true for a major version bump' do
+      mock_version_response('master', '1.2.3')
+      mock_version_response('my_branch', '2.0.0')
+
+      expect(action.version_updated?(branch: 'my_branch')).to be_truthy
     end
   end
 end
