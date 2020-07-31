e24d75240d4754231c11d6c791db3a88847241b4
Nicole Rifkin <nicole.rifkin@simplybusiness.com>
2020-07-31 14:43:03 -0400

    one env var only

--- lib/config.rb
+++ lib/config.rb
@@ -9,6 +9,6 @@ class Config
   def initialize
     @client = Octokit::Client.new(access_token: ENV['BOT_TOKEN'])
     @event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
-    @file_path = ENV['VERSION_FILE_PATH'] || ENV['GEMSPEC_PATH']
+    @file_path = ENV['VERSION_FILE_PATH']
   end
 end
