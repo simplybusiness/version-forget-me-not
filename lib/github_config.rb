e9883ed5ad5e7078c748f1072e82c600df16196e
Punit Jain <punit.jain@simplybusiness.co.uk>
2020-07-31 11:14:34 +0100

    move event to config

--- lib/github_config.rb
+++ lib/github_config.rb
@@ -9,5 +9,6 @@ class GithubConfig
   def initialize
     @client = Octokit::Client.new(access_token: ENV['BOT_TOKEN'])
     @event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
+    @file_path = ENV['VERSION_FILE_PATH'] || ENV['GEMSPEC_PATH']
   end
 end
