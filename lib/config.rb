accd586d15cf8c28799970b613ad6a1217a0b33c
Punit Jain <punit.jain@simplybusiness.co.uk>
2020-08-03 16:51:44 +0100

    use access token to create status

--- lib/config.rb
+++ lib/config.rb
@@ -7,7 +7,7 @@ class Config
   attr_reader :client, :event_payload, :file_path
 
   def initialize
-    @client = Octokit::Client.new(access_token: ENV['BOT_TOKEN'])
+    @client = Octokit::Client.new(access_token: ENV['ACCESS_TOKEN'])
     @event_payload = JSON.parse(File.read(ENV['GITHUB_EVENT_PATH']))
     @file_path = ENV['VERSION_FILE_PATH']
   end
