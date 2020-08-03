9456fe51171b0f6665f6c48171c9459310047772
Nicole Rifkin <rifkinni@users.noreply.github.com>
2020-08-03 10:50:15 -0400

    Check version from gemspec file (#4)
    * add regex for gemspec
    
    * move event to config
    
    * fix spec
    
    * rename to config (since it is not only github config)
    
    * one env var only
    
    * fix regexps
    
    * test both file formats in all cases
    
    * test regex by putting real version at the end of the fixture
    
    Co-authored-by: Punit Jain <punit.jain@simplybusiness.co.uk>

--- run.rb
+++ run.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 require_relative 'lib/action'
-require_relative 'lib/github_config'
+require_relative 'lib/config'
 
-result = Action.new(GithubConfig.new).version_changed?
+result = Action.new(Config.new).version_changed?
 puts "Version Changed: #{result}"
 raise 'Version is not changed' unless result
