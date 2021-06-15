c0fa4ddfa62fdc9e00d5b97af4cecba1957ebfa5
Adam Leach <adam.leach@simplybusiness.co.uk>
2021-06-15 12:28:01 +0100

    Fix conflicts

--- lib/action.rb
+++ lib/action.rb
@@ -7,7 +7,7 @@ class Action
   attr_reader :client, :repo, :pull_number, :head_branch, :head_commit, :base_branch, :file_path, :failed_description
 
   SEMVER_VERSION =
-    /["'](0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?["']/.freeze # rubocop:disable Layout/LineLength
+    /["'](0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?["']/ # rubocop:disable Layout/LineLength
   GEMSPEC_VERSION = Regexp.new(/\.version\s*=\s*/.to_s + SEMVER_VERSION.to_s).freeze
 
   def initialize(config)
@@ -57,7 +57,7 @@ class Action
   end
 
   def format_version(version)
-    Gem::Version.new(version[0].split('=').last.gsub(/\s/, '').gsub(/\'|\"/, ''))
+    Gem::Version.new(version[0].split('=').last.gsub(/\s/, '').gsub(/'|"/, ''))
   end
 
   def assign_pr_attributes(config)
