ea21df6515640981f0450187ecf45be6689e1f1f
Robert Peacock <robert.peacock@simplybusiness.co.uk>
2024-01-15 18:57:03 +0000

    test for open api yaml

--- spec/action_spec.rb
+++ spec/action_spec.rb
@@ -127,6 +127,12 @@ describe Action do
       expect(action.fetch_version(ref: ref)).to eq("1.2.3")
     end
 
+    it 'returns the correct version for a openapi.yaml file' do
+      mock_response('my_branch', mock_open_api_yaml_content('1.2.3'))
+
+      expect(action.fetch_version(ref: ref)).to eq("1.2.3")
+    end
+
     it 'returns the correct version for a package.json file' do
       mock_response('my_branch', mock_package_json_content('1.2.3'))
 
@@ -274,6 +280,17 @@ describe Action do
       end
     )
   end
+
+  def mock_open_api_yaml_content(version)
+    %(
+      openapi: 3.0.0
+      info:
+        title: Sample API
+        description: Optional multiline or single-line description in [CommonMark](http://commonmark.org/help/) or HTML.
+        version: "#{version}"
+    )
+  end
+
 
   def mock_package_json_content(version)
     %(
